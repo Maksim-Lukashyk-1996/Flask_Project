0c3660ec05bebcdf4e369d4748fc27778788fe31
Maksim <stik9935@gmail.com>
2022-07-04 15:54:26 +0300

    Addition to files

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text(), nullable=False)
+    date = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' %self.id
+
+# class Category(db.Model):
+#     __tablename__ = 'categories'
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(255), nullable=False)
+#     slug = db.Column(db.String(255), nullable=False)
+#     created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+#
+# def __repr__(self):
+#     return "<{}:{}>".format(id, self.name)
+#
+# class  Tag(db.Model):
+#     __tablename__ = 'tags'
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(255), nullable=False)
+#     slug = db.Column(db.String(255), nullable=False)
+#     created_on  =  db.Column(db.DateTime(), default=datetime.utcnow)
+#
+# def __repr__(self):
+#     return "<{}:{}>".format(id, self.name)
+
 
 @app.route('/')
 @app.route('/home')
